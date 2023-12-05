8d047980c2b043625dc331203837d846011b9983
Jujunipers <150176649+Jujunipers@users.noreply.github.com>
2023-12-04 20:14:11 -0500

    Animate elephant idle

--- Hungry Animal/Elephant.java
+++ Hungry Animal/Elephant.java
@@ -9,15 +9,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[8];
     
     /**
      * Constructor - The code that gets run one time when object is created
      */
     
-    public Elephant
+    public Elephant()
     {
-        setImage(idle);
+        // Set each element in the idle[] array with an idle image, load the images
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        
+        setImage(idle[0]);
+    }
+    
+    /**
+     * Animate the elephant
+     */
+    // Loop that cycles through idle[] array
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
     }
     
     public void act()
@@ -34,6 +51,9 @@ public class Elephant extends Actor
         
         // Remove apple if elephant eats it
         eat();
+        
+        // Animate the elephant
+        animateElephant();
     }
     
     /**
