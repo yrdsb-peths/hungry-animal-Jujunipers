1d62ffa3a8372f91af03910cd2fd70dda0d5e310
Jujunipers <150176649+Jujunipers@users.noreply.github.com>
2023-12-05 12:46:07 -0500

    Set new apple mage, organized files

--- Apple.java
+++ Apple.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
+    GreenfootImage apple = new GreenfootImage("images/apple1.png");
+    
     public void act()
     {
         // Apple falls downwards
